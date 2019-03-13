bf9060d35d757ca75c8338e3733446d57e56ec80
appmaker <nagarkarajinkya7@gmail.com>
2019-03-13 22:33:21 +0530

    first commit

---
+++ app/src/main/java/com/appmaker/volleyuploadimage/MainActivity.java
@@ -0,0 +1,133 @@
+package com.appmaker.volleyuploadimage;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView image;
+    Button choose, upload;
+    int PICK_IMAGE_REQUEST = 111;
+    String URL = "http://192.168.1.101/JavaRESTfullWS/DemoService/upload";
+    Bitmap bitmap;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        image = (ImageView) findViewById(R.id.image);
+        choose = (Button) findViewById(R.id.choose);
+        upload = (Button) findViewById(R.id.upload);
+
+        //opening image chooser option
+        choose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_PICK);
+                startActivityForResult(Intent.createChooser(intent, "Select Image"), PICK_IMAGE_REQUEST);
+            }
+        });
+
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (bitmap != null) {
+                    progressDialog = new ProgressDialog(MainActivity.this);
+                    progressDialog.setMessage("Uploading, please wait...");
+                    progressDialog.show();
+
+                    //converting image to base64 string
+                    try {
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                        byte[] imageBytes = baos.toByteArray();
+                        final String imageString = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+
+                        //sending image to server
+                        StringRequest request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String s) {
+                                progressDialog.dismiss();
+                                Log.v("MainActivity", "response :" + s);
+
+                                if (s.equals("true")) {
+                                    Toast.makeText(MainActivity.this, "Uploaded Successful", Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(MainActivity.this, "Some error occurred!", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError volleyError) {
+                                Toast.makeText(MainActivity.this, "Some error occurred -> " + volleyError.toString(), Toast.LENGTH_LONG).show();
+                                Log.v("MainActivity", "error : " + volleyError.toString());
+                            }
+                        }) {
+                            //adding parameters to send
+                            @Override
+                            protected Map<String, String> getParams() throws AuthFailureError {
+                                Map<String, String> parameters = new HashMap<String, String>();
+                                parameters.put("image", imageString);
+                                return parameters;
+                            }
+                        };
+
+                        RequestQueue rQueue = Volley.newRequestQueue(MainActivity.this);
+                        rQueue.add(request);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Select Image ", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri filePath = data.getData();
+
+            try {
+                //getting image from gallery
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+
+                //Setting image to ImageView
+                image.setImageBitmap(bitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
